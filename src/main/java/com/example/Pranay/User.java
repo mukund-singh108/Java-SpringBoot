c5a28fbf69991235aa41f2cb095c981d14efdf48
Mukund Singh <1999rajputmukund@gmail.com>
2026-02-10 11:04:09 +0530

    First Commit

---
+++ src/main/java/com/example/Pranay/User.java
@@ -0,0 +1,29 @@
+package com.example.Pranay;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+
+    @Column(unique = true , nullable = false)
+    private String username ;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+
+}
---
+++ src/test/java/com/example/Pranay/PranayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pranay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PranayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
